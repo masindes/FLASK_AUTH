58a39c94a0af2c7009ce9c3d618c44939e7809fe
masinde sylvester <masinde.sylvester@yahoo.com>
2025-01-24 10:12:58 +0300

    initial commit

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-from flask import flask, request, jsonify, session
-from flasl_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify, session
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask__restful import Api, Resource
+from flask_restful import Api, Resource
 from flask_cors import CORS
 from flask_session import Session
-from flask bcrypt import Bcrypt
-
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-CORS(app,supports_credentials=True)
-Bcrypt = Bcrypt(app)
+CORS(app, supports_credentials=True)
+bcrypt = Bcrypt(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -26,3 +24,28 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
+
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+        hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+        new_user = User(username=data['username'], password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message': 'User created successfully'}), 201
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User.query.filter_by(username=data['username']).first()
+        if user and bcrypt.check_password_hash(user.password, data['password']):
+            session['user_id'] = user.id
+            return jsonify({'message': 'Logged in successfully'}), 200
+        else:
+            return jsonify({'message': 'Invalid credentials'}), 401
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
