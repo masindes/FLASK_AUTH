f3c2d23eb47218700b3baaaffcfbea49bcd4dd04
masinde sylvester <masinde.sylvester@yahoo.com>
2025-01-24 09:53:50 +0300

    initial commit

--- app.py
+++ app.py
@@ -21,3 +21,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 api = Api(app)
 Session(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
